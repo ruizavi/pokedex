888e71ebbce257ccfad56e6acaee02f3c9fcc4c3
Avi <avi.ruiz@proton.me>
2023-07-05 08:31:26 -0600

    custom hook for pokemon card and added pokeball loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+import Pokeball from "./Pokeball";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <Pokeball />
+    </div>
+  );
+};
+
+export default Loader;
--- src/components/PokedexCard.tsx
+++ src/components/PokedexCard.tsx
@@ -2,39 +2,50 @@ import { useState, useEffect, useContext } from "react";
 import { Pokemon } from "../interface/pokemon";
 import api from "../libs/axios";
 import { IPokemonContext, PokemonContext } from "../context/pokemonContext";
+import Loader from "./Loader";
 
 interface PokedexRowProps {
   id: string;
 }
-const PokedexCard = ({ id }: PokedexRowProps) => {
-  const [pokemon, setPokemon] = useState<Pokemon>();
+
+const usePokemon = (id: string) => {
   const { selectPokemon } = useContext(PokemonContext) as IPokemonContext;
+  const [data, setData] = useState<Pokemon>();
+  const [isFetching, setIsFetching] = useState<boolean>(true);
 
   useEffect(() => {
     const fetching = async () => {
       const { data } = await api.get<Pokemon>(`/${id}`);
 
-      setPokemon(data);
+      setData(data);
+
+      setIsFetching(false);
     };
 
     fetching().catch();
-  }, [id]);
+  }, [id, isFetching]);
 
-  return (
-    <>
-      <div
-        className={`pokedex-card ${pokemon?.types[0].type.name ?? ""}`}
-        onClick={() => selectPokemon(pokemon as Pokemon)}
-      >
-        <img
-          src={pokemon?.sprites.front_default}
-          alt={`pokemon`}
-          title="..."
-          loading="lazy"
-        />
-        <p>{pokemon?.name}</p>
-      </div>
-    </>
+  return { data, isFetching, selectPokemon };
+};
+
+const PokedexCard = ({ id }: PokedexRowProps) => {
+  const { data, isFetching, selectPokemon } = usePokemon(id);
+
+  return isFetching ? (
+    <Loader />
+  ) : (
+    <div
+      className={`pokedex-card ${data?.types[0].type.name ?? ""}`}
+      onClick={() => selectPokemon(data as Pokemon)}
+    >
+      <img
+        src={data?.sprites.front_default}
+        alt={`pokemon`}
+        title="..."
+        loading="lazy"
+      />
+      <p>{data?.name}</p>
+    </div>
   );
 };
 
